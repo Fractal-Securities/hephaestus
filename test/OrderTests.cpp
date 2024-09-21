dd7ee343a87f2424de4e3db0d0b8d03b54ab9f8e
Matthew Leon <matthew.leon.tech@gmail.com>
2024-09-21 15:48:59 -0700

    Add support for market and limit orders
    Signed-off-by: Matthew Leon <matthew.leon.tech@gmail.com>

--- test/OrderTests.cpp
+++ test/OrderTests.cpp
@@ -1,29 +1,36 @@
 #include <gtest/gtest.h>
 
+#include <gtest/gtest.h>
+
 #include "../core/include/Order.h"
 
+// Test for creating a buy order
 TEST(OrderTest, CreateBuyOrder)
 {
 
     // Allocate buy Order
-    Order buyOrder("buy", 150.05, 10);
+    Order buyOrder("Market", "buy", 150.05, 10);
 
     // Verify Order values are set
-    EXPECT_EQ(buyOrder.getType(), "buy");
+    EXPECT_EQ(buyOrder.getType(), "Market");
+    EXPECT_EQ(buyOrder.getSide(), "buy");
     EXPECT_EQ(buyOrder.getPrice(), 150.05);
     EXPECT_EQ(buyOrder.getQuantity(), 10);
 
 }
 
+// Test for creating a sell order
 TEST(OrderTest, CreateSellOrder)
 {
 
     // Allocate sell Order
-    Order sellOrder("sell", 150.05, 10);
+    Order sellOrder("Market", "sell", 150.05, 10);
 
     // Verify Order values are set
-    EXPECT_EQ(sellOrder.getType(), "sell");
+    EXPECT_EQ(sellOrder.getType(), "Market");
+    EXPECT_EQ(sellOrder.getSide(), "sell");
     EXPECT_EQ(sellOrder.getPrice(), 150.05);
     EXPECT_EQ(sellOrder.getQuantity(), 10);
 
 }
+
