ee40505b79dbe2c4bb4d87c5b48bb7da12aea94e
Matthew Leon <matthew.leon.tech@gmail.com>
2024-09-26 14:38:38 -0700

    Add insertion and deletion priority queue features
    Signed-off-by: Matthew Leon <matthew.leon.tech@gmail.com>

--- core/src/Order.tpp
+++ core/src/Order.tpp
@@ -1,4 +1,4 @@
-#include "../include/Order.h"
+#include "Order.h"
 
 // Order Constructor
 template <typename TYPE, typename SIDE>
--- test/PriorityQueueTests.cpp
+++ test/PriorityQueueTests.cpp
@@ -2,7 +2,63 @@
 
 #include "../types/include/PriorityQueue.h"
 
-TEST(PriorityQueueTests, CreatePriorityQueue)
+TEST(PriorityQueueTests, PushValuesBid)
 {
+
+    std::vector<double> testList = {150.05, 150.00};
+
+    PriorityQueue<double, std::less<double>> bidBook;
+
+    bidBook.push(150.00);
+    bidBook.push(150.05);
+
+    ASSERT_EQ(testList, bidBook.getHeap());
+
+}
+
+TEST(PriorityQueueTests, PopValuesBid)
+{
+
+    std::vector<double> testList = {150.00};
+
+    PriorityQueue<double, std::less<double>> bidBook;
+
+    bidBook.push(150.00);
+    bidBook.push(150.05);
+
+    double val = bidBook.pop();
+
+    ASSERT_EQ(150.05, val);
+    ASSERT_EQ(testList, bidBook.getHeap());
+
+}
+
+TEST(PriorityQueueTests, PushValuesAsk)
+{
+
+    std::vector<double> testList = {150.00, 150.05};
+
+    PriorityQueue<double, std::greater<double>> askBook;
+
+    askBook.push(150.00);
+    askBook.push(150.05);
+
+    ASSERT_EQ(testList, askBook.getHeap());
+
+}
+
+TEST(PriorityQueueTests, PopValuesAsk)
+{
+
+    std::vector<double> testList = {150.05};
+
+    PriorityQueue<double, std::greater<double>> askBook;
+
+    askBook.push(150.00);
+    askBook.push(150.05);
+
+    askBook.pop();
+
+    ASSERT_EQ(testList, askBook.getHeap());
 
 }
